bf0d6d91132bf890f2116d74c55f491aaa9d8b4d
bagusjiran <105334973+bagusjiran@users.noreply.github.com>
2025-05-06 20:37:48 +0700

    Update e.kategori.php
    merubah sub kategori

--- admin/e.kategori.php
+++ admin/e.kategori.php
@@ -66,7 +66,7 @@ if (isset($_POST['simpan'])) {
 
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
-                <img src="assets/img/ytta.jpg" alt="">
+                <img src="assets/img/logo.png" alt="">
                 <span class="d-none d-lg-block">Tobag</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
